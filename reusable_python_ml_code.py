335e6025acf7c52b330da594759ed8fbfbfb714c
zaberwocky <mohammedzabiulla@gmail.com>
2022-12-04 11:01:53 -0600

    Update reusable_python_ml_code.py

--- reusable_python_ml_code.py
+++ reusable_python_ml_code.py
@@ -1,2 +1,9 @@
+#standard imports
 import pandas as pd
 from pandas import DataFrame, Series
+
+
+#-----------------------------------------------------            Summary Satistics
+
+#print unique counts in a categorical variable
+df[['<list of columns>']].nunique()
