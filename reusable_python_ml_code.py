77ed9d1e610bf1e5f502db682c0e4e12ea790a2a
zaberwocky <mohammedzabiulla@gmail.com>
2022-12-04 11:28:59 -0600

    Update reusable_python_ml_code.py

--- reusable_python_ml_code.py
+++ reusable_python_ml_code.py
@@ -15,3 +15,13 @@ is_in_df =  df[df["column"].isin(second_df['column'])]
 #Sorting 
 df.sort_values(by="column_name", ascending=False)
 
+
+#identifying pairs 
+from itertools import premutations
+
+def find_pairs(x)
+  pairs = pd.DataFrame(list(permutations(x.values, 2)), 
+                       columns = ['col_1','col_b'])
+  return pairs
+
+df.groupby('group_col')['subset_col'].apply(find_pairs)
