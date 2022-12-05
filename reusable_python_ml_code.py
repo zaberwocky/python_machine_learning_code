bc17f568634c966b0d4096c9076d7c01a46b1e94
zaberwocky <mohammedzabiulla@gmail.com>
2022-12-05 07:14:45 -0600

    Update reusable_python_ml_code.py

--- reusable_python_ml_code.py
+++ reusable_python_ml_code.py
@@ -28,3 +28,19 @@ df.groupby('group_col')['subset_col'].apply(find_pairs)
 
 #find occurences
 df.groupby(['col_1','col_2']).size()
+
+
+#crosstabulation or transposing
+pd.corsstab(df['col1'], df['col2']) # first column becomes rows and second becomes columns with 1 and 0
+
+#------------------------------------------------------               Statistics
+
+#jaccard similarity 
+from sklearn.metrics import jaccard_score
+jaccard_score(row1, row1)
+
+#finding distance between all items using Jaccard
+from scipy.distance import pdist, squareform
+jaccard_distances = pdist(df.values, metric = 'jaccard')
+square_jaccard_distances = squareform(jaccard_distances) 
+
